8c675c4f46976ef56875df39c79c2310331e2cc9
Daniel <naranjobenavidesd@gmail.com>
2024-03-14 13:47:21 -0500

    Se agrega el constructor y los metodos get and setter de la clase bebida, incluido sus atributos

--- Practica_Java_POO_1/src/clases/Bebida.java
+++ Practica_Java_POO_1/src/clases/Bebida.java
@@ -1,4 +1,26 @@
 package clases;
-
 public class Bebida {
+    private String nombre = "";
+    private String marca = "";
+    private double capacidad = 0;
+
+    //Creamos nuestro metodo constructor
+    public Bebida(String nombre, String marca, double capacidad) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.capacidad = capacidad;
+    }
+
+    //Creamos sus respectivos Get and Setter
+    public String getNombre() {return nombre;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public String getMarca() {return marca;}
+
+    public void setMarca(String marca) {this.marca = marca;}
+
+    public double getCapacidad() {return capacidad;}
+
+    public void setCapacidad(double capacidad) {this.capacidad = capacidad;}
 }
--- Practica_Java_POO_1/src/clases/Principal.java
+++ Practica_Java_POO_1/src/clases/Principal.java
@@ -1,4 +1,7 @@
 package clases;
 
 public class Principal {
+    public static void main(String[] args) {
+        String nombre = "";
+    }
 }
